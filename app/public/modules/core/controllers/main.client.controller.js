ee87b142cbe79c4a083a31d2b18b8322d7c02344
Mykyta_Shpilevskyi <Mykyta_Shpilevskyi@epam.com>
2016-03-14 11:43:07 +0200

    Create layout structure

---
+++ app/public/modules/core/controllers/main.client.controller.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('core').controller('MainController', ['$scope', 'Authentication',
+	function($scope, Authentication) {
+		// This provides Authentication context.
+		$scope.authentication = Authentication;
+
+		this.withSidebar = true;
+
+		$scope.$on('toggleSidebar', () => {
+			this.withSidebar = !this.withSidebar;
+		});
+	}
+]);

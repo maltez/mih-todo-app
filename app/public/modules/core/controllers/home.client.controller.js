57ac1654e95d552b9749dbf3b472b12b95fe50eb
Mykyta_Shpilevskyi <Mykyta_Shpilevskyi@epam.com>
2016-03-18 11:58:57 +0200

    move sidebar and header to homepage

--- app/public/modules/core/controllers/home.client.controller.js
+++ app/public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,11 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication, $location) {
 		// If user is not signed in then redirect back home
 		if (!Authentication.user) $location.path('/signin');
+
+		this.withSidebar = false;
+
+		$scope.$on('toggleSidebar', () => {
+			this.withSidebar = !this.withSidebar;
+		});
 	}
 ]);

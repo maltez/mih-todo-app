4dc9c0738ee239b1583282a9c703c6ccd02a6f89
Mykyta_Shpilevskyi <Mykyta_Shpilevskyi@epam.com>
2016-04-04 16:30:30 +0300

    Merge account settings and change password pages in one

--- app/public/modules/users/config/users.client.routes.js
+++ app/public/modules/users/config/users.client.routes.js
@@ -9,14 +9,10 @@ angular.module('users').config(['$stateProvider',
 			url: '/settings/profile',
 			templateUrl: 'modules/users/views/settings/edit-profile.client.view.html'
 		}).
-		state('password', {
-			url: '/settings/password',
-			templateUrl: 'modules/users/views/settings/change-password.client.view.html'
-		}).
-		state('accounts', {
-			url: '/settings/accounts',
-			templateUrl: 'modules/users/views/settings/social-accounts.client.view.html'
-		}).
+        state('accounts', {
+            url: '/settings/accounts',
+            templateUrl: 'modules/users/views/settings/social-accounts.client.view.html'
+        }).
 		state('signup', {
 			url: '/signup',
 			templateUrl: 'modules/users/views/authentication/signup.client.view.html'

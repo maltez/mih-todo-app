dd1a09555878e31dc17aed8a108c2d96ae4f5873
Artem Drobotenko <artem_drobotenko@epam.com>
2016-08-23 12:35:14 +0300

    [AD]. EPMKHRDMH-227. Display only work hours and filled with activities hours in the calendar

--- app/config/env/all.js
+++ app/config/env/all.js
@@ -54,6 +54,7 @@ module.exports = {
 			],
 			js: [
 				'public/lib/angular/angular.js',
+				'public/lib/lodash/lodash.js',
 				'public/lib/angular-resource/angular-resource.js',
 				'public/lib/angular-animate/angular-animate.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.js',

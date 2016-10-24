d39ceb1eab328ae50fc7a2e309a473804ec0e300
Artem Drobotenko <artem_drobotenko@epam.com>
2016-10-24 15:15:57 +0300

    heroku launch attempt #2: set prod mongo path

--- config/env/production.js
+++ config/env/production.js
@@ -2,11 +2,7 @@
 
 module.exports = {
 	db: {
-		uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
-		options: {
-			user: '',
-			pass: ''
-		}
+		uri: 'mongodb://devuser:devuser2day@ds031167.mlab.com:31167/mih'
 	},
 	log: {
 		// Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
